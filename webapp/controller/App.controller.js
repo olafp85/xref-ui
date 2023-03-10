433098291feaf9002e2e7698edb4dc65f5138634
Olaf Pohlmann <olaf.pohlmann@asml.com>
2023-03-10 17:34:18 +0100

    First commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("xref.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"xref/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
