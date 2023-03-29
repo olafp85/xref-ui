0913984019dff45c1ca650b49575fc245fa8b3d5
Olaf Pohlmann <olaf.pohlmann@asml.com>
2023-03-29 13:29:40 +0200

    Deploy progress

--- deploy.js
+++ deploy.js
@@ -3,25 +3,22 @@ const ftpDeploy = new FtpDeploy();
 
 const config = {
     user: "u149889p141504",
-    // Password optional, prompted if none given
-    password: "",
+    password: "",  // Password optional, prompted if none given
     host: "web0150.zxcs.nl",
     port: 21,
-    localRoot: __dirname,
     remoteRoot: "/domains/luukpohlmann.nl/xref-ui",
-    // include: ["*", "**/*"],      // this would upload everything except dot files
+    localRoot: __dirname,
     include: ["dist/**"],
-    // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
     exclude: ["dist/test/**"],
-    // delete ALL existing files at destination before uploading, if true
     deleteRemote: true,
-    // Passive mode is forced (EPSV command is not sent)
     forcePasv: true,
-    // use sftp or ftp
     sftp: false,
 };
 
+ftpDeploy.on("uploading", (data) => {
+    console.log("Uploading", data.filename);
+});
+
 ftpDeploy
     .deploy(config)
-    .then((res) => console.log("finished:", res))
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
